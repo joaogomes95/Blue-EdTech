718d6087c94badc07e867c96d7a0fdce141eff6e
João Vitor Gomes <joaovitorgomes95@gmail.com>
2021-08-30 17:52:41 -0300

    ok

--- Módulo 3/Aula 2/projeto02/index.js
+++ Módulo 3/Aula 2/projeto02/index.js
@@ -5,9 +5,6 @@ const app = express();
 
 const port = 3003;
 
-app.use(express.json());
-
-
 const games = [
     'Valorant',
     'League of Legends',
@@ -51,67 +48,9 @@ app.get('/games',(req, res) => {
 app.get('/games/:id',(req, res) => {
     const id = req.params.id -1;
     const game = games[id];
-
-    if(!game){
-        res.send('Game não encontrado');
-    }
-
     res.send(game);
-});
-//-----------------------------------------------------------------------------------------
-//Inicio do CRUD
-//Post
-app.post('/games', (req, res)=>{
-    const game = req.body.game;
-    const id = games.length +1;
+})
 
-    games.push(game);
-
-    res.send(`Jogo adicionado com sucesso: ${game} e seu id é ${id}.`);
-});
-
-// Atualização / put
-
-// app.put('/games/:id',(req, res)=>{
-//     const id = req.params.id -1;
-//     const game = req.body.game;
-//     const gameAntigo = games[id];
-//     games[id] = game;
-
-//     res.send(`O jogo ${gameAntigo} foi atualizado para ${game}.`);
-// });
-
-app.put('/games/:id',(req, res)=>{
-    const id = req.params.id-1;
-    const game = req.body.game;
-
-    if (!games[id]){
-        res.send("Jogo não encontrato.");
-    }
-    
-    else{
-        const nomeAnterior = games[id];
-        games[id] = game;
-        res.send(`Jogo ${nomeAnterior} atualizado com sucesso: ${game}.`);
-    }
-});
-
-
-//Delete ---------------------------------------------
-app.delete ('/games/:id', (req, res)=>{
-    const id = req.params.id -1;
-    const game = games[id];
-    
-    if(!game){
-        res.send('O jogo não foi encontrado');
-    }
-
-    delete games[id];
-    //O res.send funciona igual ao return mandando uma resposta ao usuário final.
-    res.send("O jogo foi deletado")
-});
-
-//--------------------------------------------------
 app.listen(port, (req, res) => {
     console.info(`App está rodando na porta http://localhost/${port}/`)
 });
