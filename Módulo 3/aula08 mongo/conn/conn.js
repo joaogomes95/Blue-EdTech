18a7a4847b07a0eb2caf2270008d4c6e0a95474c
João Vitor Gomes <joaovitorgomes95@gmail.com>
2021-08-31 19:49:59 -0300

    Mongo DB

---
+++ Módulo 3/aula08 mongo/conn/conn.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+// url de conexao -> mongodb://servidor:porta/nome do banco
+// useNewUrlParser --> Para que o Moongoose use o novo sistema de Url
+// useUnifiedTopology --> Mecanismo de monitoramento de Banco de Dados
+
+function Conn(url, porta, banco){
+    mongoose.connect(`mongodb://${url}:${porta}/${banco}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(()=>{
+        console.log("MongoDB conectado");        
+    }).catch((err)=>{
+        console.error(err);
+    });
+}
+
+
+module.exports = Conn;
---
+++ Módulo 3/aula08 mongo/routes/usuarios.routes.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+// const router = express.Router();
+
+// // Rota que lista todos os usuários que
+
+// router.get('/usuarios', () => {
+
+// })
+
+
+// module.exports = router;
